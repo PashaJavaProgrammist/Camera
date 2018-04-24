d24ddac39467ec06f8a1f0163fb16e6a2476d5ea
Pavel <pasha280292@gmail.com>
2018-04-24 16:34:15 +0300

    minor changes

--- app/src/main/java/com/haretskiy/pavel/magiccamera/ui/fragments/Camera2Fragment.kt
+++ app/src/main/java/com/haretskiy/pavel/magiccamera/ui/fragments/Camera2Fragment.kt
@@ -647,8 +647,61 @@ class Camera2Fragment : Fragment(), View.OnClickListener {
         PermissionDialog().show(childFragmentManager, FRAGMENT_DIALOG_COMP)
     }
 
-    companion object {
+    /**
+     * Given `choices` of `Size`s supported by a camera, choose the smallest one that
+     * is at least as large as the respective texture view size, and that is at most as large as
+     * the respective max size, and whose aspect ratio matches with the specified value. If such
+     * size doesn't exist, choose the largest one that is at most as large as the respective max
+     * size, and whose aspect ratio matches with the specified value.
+     *
+     * @param choices           The list of sizes that the camera supports for the intended
+     *                          output class
+     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
+     * @param textureViewHeight The height of the texture view relative to sensor coordinate
+     * @param maxWidth          The maximum width that can be chosen
+     * @param maxHeight         The maximum height that can be chosen
+     * @param aspectRatio       The aspect ratio
+     * @return The optimal `Size`, or an arbitrary one if none were big enough
+     */
+    private fun chooseOptimalSize(
+            choices: Array<Size>,
+            textureViewWidth: Int,
+            textureViewHeight: Int,
+            maxWidth: Int,
+            maxHeight: Int,
+            aspectRatio: Size
+    ): Size {
 
+        // Collect the supported resolutions that are at least as big as the preview Surface
+        val bigEnough = ArrayList<Size>()
+        // Collect the supported resolutions that are smaller than the preview Surface
+        val notBigEnough = ArrayList<Size>()
+        val w = aspectRatio.width
+        val h = aspectRatio.height
+        for (option in choices) {
+            if (option.width <= maxWidth && option.height <= maxHeight &&
+                    option.height == option.width * h / w) {
+                if (option.width >= textureViewWidth && option.height >= textureViewHeight) {
+                    bigEnough.add(option)
+                } else {
+                    notBigEnough.add(option)
+                }
+            }
+        }
+
+        // Pick the smallest of those big enough. If there is no one big enough, pick the
+        // largest of those not big enough.
+        return when {
+            bigEnough.size > 0 -> Collections.min(bigEnough, ComparatorSizesByArea())
+            notBigEnough.size > 0 -> Collections.max(notBigEnough, ComparatorSizesByArea())
+            else -> {
+                Log.e(TAG, "Couldn't find any suitable preview size")
+                choices[0]
+            }
+        }
+    }
+
+    companion object {
         /**
          * Conversion from screen rotation to JPEG orientation.
          */
@@ -660,66 +713,5 @@ class Camera2Fragment : Fragment(), View.OnClickListener {
             ORIENTATIONS.append(Surface.ROTATION_180, 270)
             ORIENTATIONS.append(Surface.ROTATION_270, 180)
         }
-
-        /**
-         * Tag for the [Log].
-         */
-
-
-        /**
-         * Given `choices` of `Size`s supported by a camera, choose the smallest one that
-         * is at least as large as the respective texture view size, and that is at most as large as
-         * the respective max size, and whose aspect ratio matches with the specified value. If such
-         * size doesn't exist, choose the largest one that is at most as large as the respective max
-         * size, and whose aspect ratio matches with the specified value.
-         *
-         * @param choices           The list of sizes that the camera supports for the intended
-         *                          output class
-         * @param textureViewWidth  The width of the texture view relative to sensor coordinate
-         * @param textureViewHeight The height of the texture view relative to sensor coordinate
-         * @param maxWidth          The maximum width that can be chosen
-         * @param maxHeight         The maximum height that can be chosen
-         * @param aspectRatio       The aspect ratio
-         * @return The optimal `Size`, or an arbitrary one if none were big enough
-         */
-        @JvmStatic
-        private fun chooseOptimalSize(
-                choices: Array<Size>,
-                textureViewWidth: Int,
-                textureViewHeight: Int,
-                maxWidth: Int,
-                maxHeight: Int,
-                aspectRatio: Size
-        ): Size {
-
-            // Collect the supported resolutions that are at least as big as the preview Surface
-            val bigEnough = ArrayList<Size>()
-            // Collect the supported resolutions that are smaller than the preview Surface
-            val notBigEnough = ArrayList<Size>()
-            val w = aspectRatio.width
-            val h = aspectRatio.height
-            for (option in choices) {
-                if (option.width <= maxWidth && option.height <= maxHeight &&
-                        option.height == option.width * h / w) {
-                    if (option.width >= textureViewWidth && option.height >= textureViewHeight) {
-                        bigEnough.add(option)
-                    } else {
-                        notBigEnough.add(option)
-                    }
-                }
-            }
-
-            // Pick the smallest of those big enough. If there is no one big enough, pick the
-            // largest of those not big enough.
-            return when {
-                bigEnough.size > 0 -> Collections.min(bigEnough, ComparatorSizesByArea())
-                notBigEnough.size > 0 -> Collections.max(notBigEnough, ComparatorSizesByArea())
-                else -> {
-                    Log.e(TAG, "Couldn't find any suitable preview size")
-                    choices[0]
-                }
-            }
-        }
-
     }
 }
