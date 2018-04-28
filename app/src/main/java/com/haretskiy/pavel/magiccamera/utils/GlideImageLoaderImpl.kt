31fd2160857beecb53d4be5f588e830a20ca205a
Pavel <pasha280292@gmail.com>
2018-04-29 01:29:11 +0300

    glide added and round photo on preview

--- app/src/main/java/com/haretskiy/pavel/magiccamera/utils/GlideImageLoaderImpl.kt
+++ app/src/main/java/com/haretskiy/pavel/magiccamera/utils/GlideImageLoaderImpl.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.haretskiy.pavel.magiccamera.R
 import com.haretskiy.pavel.magiccamera.utils.interfaces.ImageLoader
 
 class GlideImageLoaderImpl(private val context: Context) : ImageLoader {
@@ -14,8 +15,9 @@ class GlideImageLoaderImpl(private val context: Context) : ImageLoader {
                 .load(uri)
                 .apply(RequestOptions
                         .circleCropTransform()
-                        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC))
-//              .placeholder(R.drawable.loading_spinner)
+                        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                        .placeholder(R.mipmap.ic_launcher_round)
+                        .error(R.mipmap.ic_launcher_round))
                 .into(imageView)
     }
 }
