178354e5274768c3cdddf6ea7d489c59b5b591dc
Pavel <pasha280292@gmail.com>
2018-05-03 12:22:20 +0300

    settings refs

--- app/src/main/java/com/haretskiy/pavel/magiccamera/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/haretskiy/pavel/magiccamera/ui/fragments/SettingsFragment.kt
@@ -1,26 +1,19 @@
 package com.haretskiy.pavel.magiccamera.ui.fragments
 
-
 import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.haretskiy.pavel.magiccamera.R
 import com.haretskiy.pavel.magiccamera.SIGN_OUT_CODE
-import com.haretskiy.pavel.magiccamera.utils.Prefs
-import com.haretskiy.pavel.magiccamera.utils.interfaces.Router
 import com.haretskiy.pavel.magiccamera.viewModels.SettingsViewModel
 import kotlinx.android.synthetic.main.fragment_settings.*
 import org.koin.android.ext.android.inject
 
-
 class SettingsFragment : Fragment() {
 
-    private val router: Router by inject()
-    private val prefs: Prefs by inject()
     private val settingsViewModel: SettingsViewModel by inject()
 
     val data = settingsViewModel.userInfo.observe(this, Observer {
@@ -37,18 +30,16 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         settings_progress_bar.visibility = View.GONE
-        bt_settings.setOnClickListener({ router.startSettingsActivity() })
+        bt_settings.setOnClickListener({ settingsViewModel.startGoToSettings() })
         bt_log_out.setOnClickListener({
             settings_progress_bar.visibility = View.VISIBLE
             settingsViewModel.logOut()
         })
     }
 
-    //todo: move this in view model
     private fun signOut() {
         settings_progress_bar.visibility = View.GONE
-        prefs.setUserStateLogOut()
-        router.startLoginActivity()
+        settingsViewModel.signOut()
     }
 
 }
--- app/src/main/java/com/haretskiy/pavel/magiccamera/viewModels/SettingsViewModel.kt
+++ app/src/main/java/com/haretskiy/pavel/magiccamera/viewModels/SettingsViewModel.kt
@@ -4,22 +4,32 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.haretskiy.pavel.magiccamera.SIGN_OUT_CODE
+import com.haretskiy.pavel.magiccamera.utils.Prefs
+import com.haretskiy.pavel.magiccamera.utils.interfaces.Router
 
-class SettingsViewModel(private val mAuth: FirebaseAuth) : ViewModel() {
+class SettingsViewModel(private val mAuth: FirebaseAuth,
+                        private val prefs: Prefs,
+                        private val router: Router) : ViewModel() {
 
     val userInfo: MutableLiveData<String> = MutableLiveData()
 
     fun logOut() {
         mAuth.currentUser
-        mAuth.addAuthStateListener(object : FirebaseAuth.AuthStateListener {
-            override fun onAuthStateChanged(firebaseAuth: FirebaseAuth) {
-                if (firebaseAuth.currentUser == null) {
-                    userInfo.postValue(SIGN_OUT_CODE)
-                }
+        mAuth.addAuthStateListener { firebaseAuth ->
+            if (firebaseAuth.currentUser == null) {
+                userInfo.postValue(SIGN_OUT_CODE)
             }
-        })
+        }
         mAuth.signOut()
+    }
 
+    fun signOut() {
+        prefs.setUserStateLogOut()
+        router.startLoginActivity()
+    }
+
+    fun startGoToSettings() {
+        router.startSettingsActivity()
     }
 
 }
